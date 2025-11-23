6a39011524e0f23ed4ff3379a3567d3bf32f1c82
tencent_4c4g <1461108695@qq.com>
2025-11-23 20:43:49 +0800

    [fix] remove unnecessary gomod

--- auth_service/controller/auth_grpc.go
+++ auth_service/controller/auth_grpc.go
@@ -2,9 +2,14 @@ package controller
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/Chateaubriand-g/bili/auth_service/dao"
+	"github.com/Chateaubriand-g/bili/auth_service/middleware"
+	"github.com/Chateaubriand-g/bili/common/model"
 	"github.com/Chateaubriand-g/bili/pkg/authpb"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type GRPCServer struct {
@@ -17,5 +22,17 @@ func NewGRPCServer(dao dao.UserDAO) *GRPCServer {
 }
 
 func (s *GRPCServer) Login(ctx context.Context, req *authpb.LoginRequest) (*authpb.LoginResponse, error) {
+	var user *model.User
+	user, err := s.dao.FindByUsername(req.username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	if bcrypt.CompareHashAndPassword([]byte(user.PassWord), []byte(in.Password)) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "password error"})
+		return
+	}
+	accesstoken, _ := middleware.GenerateAccessToken(uint64(user.ID), "abc")
+	refreshtoken, _ := middleware.GenerateRefreshToken(uint64(user.ID), "abc")
 
 }
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Chateaubriand-g/bili
-
-go 1.25.4
